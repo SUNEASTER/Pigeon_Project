669fdc36d0794cb9d98566e4b3be28ed1b8bdb29
FooLearn <99015278+FooLearn@users.noreply.github.com>
2022-09-23 14:54:16 +0700

    Update userModel.php

--- models/userModel.php
+++ models/userModel.php
@@ -6,87 +6,143 @@
         public $User_Id;
         public $Password;
         public $Email;
+        public $Count;
 
-        public function __construct($Open_Id, $User_Id, $Password, $Email){
+        public function __construct($Open_Id, $User_Id, $Password, $Email, $Count){
             $this->Open_Id = $Open_Id;
             $this->User_Id = $User_Id;
             $this->Password = $Password;
             $this->Email = $Email;
+            $this->Count = $Count;
         }
 
         public static function getByOpenID($Open_ID){
             try {
                 require("connectionConnect.php");
-                $tsql = "SELECT TOP(1) * FROM useraccount where userId = 'pao' AND password = '1234'";
-                $getProducts = sqlsrv_query($conn, $tsql);
-                if ($getProducts == FALSE)
+                $tsql = "SELECT TOP(1) * FROM useraccount where userOpenId = $Open_ID";
+                $getUser = sqlsrv_query($conn, $tsql);
+                if ($getUser == FALSE)
                     die(FormatErrors(sqlsrv_errors()));
-                $productCount = 0;
-                while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+                $Count = 0;
+                while($row = sqlsrv_fetch_array($getUser, SQLSRV_FETCH_ASSOC))
                 {
                     $Open_Id = $row['userOpenId'];
                     $User_Id = $row['userId'];
                     $Password = $row['password'];
                     $Email = $row['email'];
+                    $Count++;
                 }
-                sqlsrv_free_stmt($getProducts);
+                sqlsrv_free_stmt($getUser);
                 sqlsrv_close($conn);
-                return new User($Open_Id, $User_Id, $Password, $Email);
+                if ($Count == 0)
+                    return null;
+                return new User($Open_Id, $User_Id, $Password, $Email, $Count);
             }
-            catch(Exception $e)
-            {
-                echo("Error!");
+            catch(Exception $e) {
                 return null;
             }
-            // require("connectionConnect.php");
-            // $sql = "SELECT * FROM useraccount WHERE useraccount.openID = '$Open_ID'";
-            // $result = $conn->query($sql);
-            // $my_row = $result->fetch_assoc();
-            // if( is_null($my_row) ) {
-            //     require("connectionClose.php");
-            //     return null;
-            // }
-            // $Open_Id = $my_row["openID"];
-            // $User_Id = $my_row["userID"];
-            // $Password = $my_row["password"];
-            // $FirstName = $my_row["firstName"];
-            // $LastName = $my_row["lastName"];
-            // $Student_Id = $my_row["studentID"];
-            // $Student_Faculty = $my_row["studentFaculty"];
-            // $Student_Branch = $my_row["studentBranch"];
-            // $Student_Status = $my_row["studentStatus"];
-            // $YOS = $my_row["YOS"];
-            // $Gender = $my_row["gender"];
-            // $User_Role = $my_row["userRole"];
-
-            // require("connectionClose.php");
-            //return new User($Open_Id, $User_Id, $Password, $FirstName, $LastName, $Student_Id, $Student_Faculty, $Student_Branch, $Student_Status, $YOS, $Gender, $User_Role);
         }
 
-        // public static function getByUserID($User_ID){
-        //     require("connectionConnect.php");
-        //     $sql = "SELECT * FROM useraccount WHERE useraccount.userID = '$User_ID'";
-        //     $result = $conn->query($sql);
-        //     $my_row = $result->fetch_assoc();
-        //     if( is_null($my_row) ) {
-        //         require("connectionClose.php");
-        //         return null;
-        //     }
-        //     $Open_Id = $my_row["openID"];
-        //     $User_Id = $my_row["userID"];
-        //     $Password = $my_row["password"];
-        //     $FirstName = $my_row["firstName"];
-        //     $LastName = $my_row["lastName"];
-        //     $Student_Id = $my_row["studentID"];
-        //     $Student_Faculty = $my_row["studentFaculty"];
-        //     $Student_Branch = $my_row["studentBranch"];
-        //     $Student_Status = $my_row["studentStatus"];
-        //     $YOS = $my_row["YOS"];
-        //     $Gender = $my_row["gender"];
-        //     $User_Role = $my_row["userRole"];
+        public static function getByUserId($UserId, $Password){
+            try {
+                require("connectionConnect.php");
+                $tsql = "SELECT TOP(1) * FROM useraccount where userId = '$UserId' AND password = '$Password'";
+                $getUser = sqlsrv_query($conn, $tsql);
+                if ($getUser == FALSE)
+                    die(FormatErrors(sqlsrv_errors()));
+                $Count = 0;
+                while($row = sqlsrv_fetch_array($getUser, SQLSRV_FETCH_ASSOC))
+                {
+                    $Open_Id = $row['userOpenId'];
+                    $User_Id = $row['userId'];
+                    $Password = $row['password'];
+                    $Email = $row['email'];
+                    $Count++;
+                }
+                sqlsrv_free_stmt($getUser);
+                sqlsrv_close($conn);
+                if ($Count == 0)
+                    return null;
+                return new User($Open_Id, $User_Id, $Password, $Email, $Count);
+            }
+            catch(Exception $e) {
+                return null;
+            }
+        }
 
-        //     require("connectionClose.php");
-        //     return new User($Open_Id, $User_Id, $Password, $FirstName, $LastName, $Student_Id, $Student_Faculty, $Student_Branch, $Student_Status, $YOS, $Gender, $User_Role);   
-        // }
+        public static function getByUserId($UserId){
+            try {
+                require("connectionConnect.php");
+                $tsql = "SELECT TOP(1) * FROM useraccount where userId = '$UserId'";
+                $getUser = sqlsrv_query($conn, $tsql);
+                if ($getUser == FALSE)
+                    die(FormatErrors(sqlsrv_errors()));
+                $Count = 0;
+                while($row = sqlsrv_fetch_array($getUser, SQLSRV_FETCH_ASSOC))
+                {
+                    $Open_Id = $row['userOpenId'];
+                    $User_Id = $row['userId'];
+                    $Password = $row['password'];
+                    $Email = $row['email'];
+                    $Count++;
+                }
+                sqlsrv_free_stmt($getUser);
+                sqlsrv_close($conn);
+                if ($Count == 0)
+                    return null;
+                return new User($Open_Id, $User_Id, $Password, $Email, $Count);
+            }
+            catch(Exception $e) {
+                return null;
+            }
+        }
+
+        public static function getByEmail($Email){
+            try {
+                require("connectionConnect.php");
+                $tsql = "SELECT TOP(1) * FROM useraccount where email = '$Email'";
+                $getUser = sqlsrv_query($conn, $tsql);
+                if ($getUser == FALSE)
+                    die(FormatErrors(sqlsrv_errors()));
+                $Count = 0;
+                while($row = sqlsrv_fetch_array($getUser, SQLSRV_FETCH_ASSOC))
+                {
+                    $Open_Id = $row['userOpenId'];
+                    $User_Id = $row['userId'];
+                    $Password = $row['password'];
+                    $Email = $row['email'];
+                    $Count++;
+                }
+                sqlsrv_free_stmt($getUser);
+                sqlsrv_close($conn);
+                if ($Count == 0)
+                    return null;
+                return new User($Open_Id, $User_Id, $Password, $Email, $Count);
+            }
+            catch(Exception $e) {
+                return null;
+            }
+        }
+
+        public static function addUser($UserId, $Password){
+            try{
+                require("connectionConnect.php");
+
+                $tsql = "INSERT INTO useraccount (userId, [password], email)"
+                ." VALUES ('test', '1234', 'asd@gmail.com')";
+                $insertUser = sqlsrv_query($conn, $tsql);
+                if($insertUser == FALSE)
+                    die(FormatErrors( sqlsrv_errors()));
+                echo "Product Key inserted is :";
+                // while($row = sqlsrv_fetch_array($insertUser, SQLSRV_FETCH_ASSOC))
+                // {
+                //     echo($row['userOpenId']);
+                // }
+                sqlsrv_free_stmt($insertReview);
+                sqlsrv_close($conn);
+            }
+            catch(Exception $e){
+            }
+        }
 
     }
