3b1f66ef754cf48595ace5dcf84e7c0f85a25cec
FooLearn <99015278+FooLearn@users.noreply.github.com>
2022-09-25 21:25:12 +0700

    Update connectionConnect.php

--- connectionConnect.php
+++ connectionConnect.php
@@ -1,7 +1,7 @@
 <?php
 $serverName = "tcp:pigeon-test.database.windows.net,1433";
 $connectionOptions = array("Database"=>"pigeon",
-    "Uid"=>"pigeon_admin", "PWD"=>"password_1");
+    "Uid"=>"pigeon_admin", "PWD"=>"password_1", "CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 if($conn == false)
     die(FormatErrors(sqlsrv_errors()));
